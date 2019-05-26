bdf46066eb09e8c375bf23da42a516b9dc55206a
Petter Andersson <petter@pandersson.net>
2019-05-26 12:34:15 +0200

    Sort bookings on from date descending

--- app/booking_router.js
+++ app/booking_router.js
@@ -1,27 +1,27 @@
-const router = require('express').Router();
-const Booking = require('./models/Booking');
+const router = require("express").Router();
+const Booking = require("./models/Booking");
 
-const isLoggedIn = require('./AuthHelpers').isLoggedIn;
+const isLoggedIn = require("./AuthHelpers").isLoggedIn;
 
-router.get('/', (req, res) => {
-   Booking.find({}, (err, bookings) => {
+router.get("/", (req, res) => {
+   Booking.find({}, null, { sort: "-from" }, (err, bookings) => {
       bookings = bookings.map(booking => {
          return {
             from: booking.from,
             to: booking.to,
             user: booking.user,
             keyId: booking.keyId
-         }
+         };
       });
       res.status(200).send(bookings);
    });
 });
 
-router.post('/', isLoggedIn, (req, res) => {
+router.post("/", isLoggedIn, (req, res) => {
    const bookings = req.body;
    if (bookings && req.user) {
       const newBookings = bookings.map(booking => {
-         const rand = Math.floor(Math.random() * Math.floor(10000))
+         const rand = Math.floor(Math.random() * Math.floor(10000));
          return {
             from: booking.from,
             to: booking.to,
@@ -29,10 +29,12 @@ router.post('/', isLoggedIn, (req, res) => {
                name: req.user.name,
                username: req.user.username
             }
-         }
+         };
       });
       Booking.create(newBookings, (err, bookings) => {
-         if (err) { return res.status(500).send(err) }
+         if (err) {
+            return res.status(500).send(err);
+         }
          res.status(201).send(bookings);
       });
    } else {
@@ -40,17 +42,16 @@ router.post('/', isLoggedIn, (req, res) => {
    }
 });
 
-
-router.delete('/:id', isLoggedIn, (req, res) => {
+router.delete("/:id", isLoggedIn, (req, res) => {
    const bookingId = req.params.id;
-   Booking.deleteOne({ keyId: bookingId }, (err) => {
-      if (err) { return res.status(500).send(err); }
-      res.status(200).send({ "deleted": bookingId });
+   Booking.deleteOne({ keyId: bookingId }, err => {
+      if (err) {
+         return res.status(500).send(err);
+      }
+      res.status(200).send({ deleted: bookingId });
    });
 });
 
-router.put('/:id', isLoggedIn, (req, res) => {
-
-});
+router.put("/:id", isLoggedIn, (req, res) => {});
 
 module.exports = router;
